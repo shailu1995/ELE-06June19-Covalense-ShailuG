312d6cffbdc87d331509f69facf076c1a771a9a7
Shailu <shailugadmi421@gmail.com>
2019-06-26 18:12:12 +0530

    programs on array

---
+++ COREJAVA/src/com/covalense/javaapp/array/TestC.java
@@ -0,0 +1,17 @@
+package com.covalense.javaapp.array;
+
+public class TestC {
+	public static void main(String[] args) {
+		String[] s=new String[3];
+		s[0]="shailu";
+		s[1]="Gadmi";
+		s[2]="chitguppa";
+		
+		for(int i=0;i<s.length;i++) {
+			System.out.println(s[i]);
+		}
+		
+		//System.out.println(s.length);
+	}
+
+}
---
+++ COREJAVA/src/com/covalense/javaapp/array/TestD.java
@@ -0,0 +1,18 @@
+package com.covalense.javaapp.array;
+
+public class TestD {
+	public static void main(String[] args) {
+		
+		int[] a= {1,2,3,4};
+		for(int i=0;i<4;i++) {
+			System.out.println(a[i]);
+			}
+		System.out.println("***********");
+		
+		char[] c= {'s','w','a','t','i'};
+		for(int i=0;i<c.length;i++) {
+			System.out.println(c[i]);
+		}
+	}
+}
+
