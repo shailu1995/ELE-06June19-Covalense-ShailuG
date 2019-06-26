80dfe8d8a4eb2b2c148fa3ebbedba053d5bb5983
Shailu <shailugadmi421@gmail.com>
2019-06-26 09:36:14 +0530

    array programs

---
+++ COREJAVA/src/com/covalense/javaapp/array/Test1.java
@@ -0,0 +1,16 @@
+package com.covalense.javaapp.array;
+
+public class Test1 {
+	public static void main(String[] args) {
+		double[] b=new double[4];
+		b[0]=1.2;
+		b[1]=2.2;
+		b[2]=4.2;
+		b[3]=6.2;
+		for(int i=0;i<4;i++)
+		{
+			System.out.println(b[i]);
+		}
+	}
+
+}
---
+++ COREJAVA/src/com/covalense/javaapp/array/Test2.java
@@ -0,0 +1,16 @@
+package com.covalense.javaapp.array;
+
+public class Test2 {
+	public static void main(String[] args) {
+		
+		char[] c=new char[5];
+		c[0]='s';
+		c[1]='w';
+		c[2]='a';
+		c[3]='t';
+		c[4]='i';
+		for(int i=0;i<5;i++) {
+			System.out.println(c[i]);
+			}
+		}
+	}
