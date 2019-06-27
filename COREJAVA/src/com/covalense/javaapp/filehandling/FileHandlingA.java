115deb99cdc78bf0d0d34424b153026f98e0eb01
Shailu <shailugadmi421@gmail.com>
2019-06-27 09:35:52 +0530

    worked examples on file handling

--- COREJAVA/src/com/covalense/javaapp/filehandling/FileHandlingA.java
+++ COREJAVA/src/com/covalense/javaapp/filehandling/FileHandlingA.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
 
-public class FileHandling1 {
+public class FileHandlingA {
 
 	public static void main(String[] args) {
 		System.out.println("Reading the file..");
