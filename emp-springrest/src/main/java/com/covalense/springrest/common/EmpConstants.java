90d46936eb814ccd93518d46eeb5bd543b9cb71a
Shailu <shailugadmi421@gmail.com>
2019-08-14 18:49:52 +0530

    d

--- emp-springrest/src/main/java/com/covalense/springrest/common/EmpConstants.java
+++
@@ -1,9 +0,0 @@
-package com.covalense.springrest.common;
-
-public interface EmpConstants {
-	String VIEW_LOGINPAGE="loginPage";
-	String PROPS_EMP="classpath:emp.properties";
-	String REGISTER="createprofile";
-
-}
-
